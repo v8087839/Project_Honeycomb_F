e3984f63871950c2624c0a7c6b15d4f2d96896ac
MiniNinja227 <simonspalding1999@gmail.com>
2022-03-01 14:53:11 +0000

    Added Initial Files and added tabs with icons and names

---
+++ app/src/main/java/uk/ac/tees/v8087839/project_honeycomb_f/MainMenu.java
@@ -0,0 +1,45 @@
+package uk.ac.tees.v8087839.project_honeycomb_f;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.tabs.TabLayout;
+
+import androidx.viewpager.widget.ViewPager;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import uk.ac.tees.v8087839.project_honeycomb_f.ui.main.SectionsPagerAdapter;
+import uk.ac.tees.v8087839.project_honeycomb_f.databinding.ActivityMainMenuBinding;
+
+public class MainMenu extends AppCompatActivity {
+
+    private ActivityMainMenuBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainMenuBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+        ViewPager viewPager = binding.viewPager;
+        viewPager.setAdapter(sectionsPagerAdapter);
+        TabLayout tabs = binding.tabs;
+        tabs.setupWithViewPager(viewPager);
+        FloatingActionButton fab = binding.fab;
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+}
