2dd2f5226a8842acab1ed5634ded8b6554a8df9a
w9093284 <Password>
2022-03-29 14:17:48 +0100

    Linked UI elements to Variables, Made the two buttons go to the main menu

--- app/src/main/java/uk/ac/tees/v8087839/project_honeycomb_f/Feedback.java
+++ app/src/main/java/uk/ac/tees/v8087839/project_honeycomb_f/Feedback.java
@@ -2,7 +2,9 @@ package uk.ac.tees.v8087839.project_honeycomb_f;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -10,14 +12,41 @@ public class Feedback extends AppCompatActivity
 {
     //Initialising UI Elements
     Button Send;
-    Button Back;
+    Button Cancel;
     TextView FeedbackHead;
     TextView FeedbackBody;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedback);
+
+        //Assigning Variables to their respective UI elements
+        Send.findViewById(R.id.Send);
+        Cancel.findViewById(R.id.Cancel);
+        FeedbackHead.findViewById(R.id.FeedbackHead);
+        FeedbackBody.findViewById(R.id.FeedbackBody);
+
+        Send.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+
+                startActivity(new Intent(getApplicationContext(), MainMenu.class));
+            }
+        });
+
+        Cancel.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+
+                startActivity(new Intent(getApplicationContext(), MainMenu.class));
+            }
+        });
     }
 
 
