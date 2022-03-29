9ec89e3508b5ff4f26de2a031b73a4b58deceff7
w9093284 <Password>
2022-03-29 13:33:23 +0100

    Added Bugs and Feedback Pages/Layout files, added UI features in Feedback File, Added Title and Description Strings and Text views in Layout File

---
+++ app/src/main/java/uk/ac/tees/v8087839/project_honeycomb_f/Feedback.java
@@ -0,0 +1,25 @@
+package uk.ac.tees.v8087839.project_honeycomb_f;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Feedback extends AppCompatActivity
+{
+
+    Button Send;
+    Button Back;
+    TextView FeedbackHead;
+    TextView FeedbackBody;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feedback);
+    }
+
+
+}
---
+++ app/src/main/java/uk/ac/tees/v8087839/project_honeycomb_f/ReportBugs.java
@@ -0,0 +1,14 @@
+package uk.ac.tees.v8087839.project_honeycomb_f;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReportBugs extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_report_bugs);
+    }
+}
