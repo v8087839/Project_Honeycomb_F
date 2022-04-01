71aa036bca965cfe1555c6d78e4ae64b6cb7f98f
w9093284 <Password>
2022-04-01 14:10:11 +0100

    Main menu reworked, no longer coordinator layout, is instead a Constraint layout, all things concerning fragments and view pagers removed

--- app/src/main/java/uk/ac/tees/v8087839/project_honeycomb_f/MainMenu.java
+++ app/src/main/java/uk/ac/tees/v8087839/project_honeycomb_f/MainMenu.java
@@ -2,22 +2,12 @@ package uk.ac.tees.v8087839.project_honeycomb_f;
 
 import android.content.Intent;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.tabs.TabLayout;
 import com.google.firebase.auth.FirebaseAuth;
-
-import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
-
-//import android.view.Menu;
-//import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
-import uk.ac.tees.v8087839.project_honeycomb_f.ui.main.SectionsPagerAdapter;
-import uk.ac.tees.v8087839.project_honeycomb_f.databinding.ActivityMainMenuBinding;
+
 
 public class MainMenu extends AppCompatActivity {
 
@@ -26,26 +16,20 @@ public class MainMenu extends AppCompatActivity {
     Button Bugs;
     Button Settings;
     Button SignOut;
-    private ActivityMainMenuBinding binding;
+    Button Timetable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
 
-        binding = ActivityMainMenuBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
-        ViewPager viewPager = binding.viewPager;
-        viewPager.setAdapter(sectionsPagerAdapter);
-        TabLayout tabs = binding.tabs;
-        tabs.setupWithViewPager(viewPager);
 
         SignOut = findViewById(R.id.SignOut);
         editProfile = findViewById(R.id.editProfile);
         Feedback = findViewById(R.id.Feedback);
         Bugs = findViewById(R.id.Bugs);
         Settings = findViewById(R.id.Settings);
+        Timetable = findViewById(R.id.TimetableButton);
 
         //Settings access
         Settings.setOnClickListener(new View.OnClickListener()
@@ -96,6 +80,15 @@ public class MainMenu extends AppCompatActivity {
                 FirebaseAuth.getInstance().signOut();
                 startActivity(new Intent(getApplicationContext(), HomeActivity.class));
                 finish();
+            }
+        });
+
+        Timetable.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                startActivity(new Intent(getApplicationContext(), Timetable.class));
             }
         });
     }
